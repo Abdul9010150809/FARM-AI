3334ddf8bd23b8ebe09ef7a62aad56664978de09
Shaik.Abdul Sammed <samm41236@gmail.com>
2025-10-12 15:39:09 +0530

    add

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-
+import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
